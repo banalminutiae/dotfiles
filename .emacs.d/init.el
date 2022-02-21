4bb1b27026ab8c83628529e6ac89f0d6243833ad
Andrew Li <andrewli-1920@hotmail.com>
2022-02-21 10:51:02 +0800

    zap-up-to-char on 'b'; left hand editing right hand navigation always.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -157,7 +157,7 @@
 
 (setq compile-command "build.bat")
 (define-key xah-fly-command-map (kbd "q") 'goto-line)
-(define-key xah-fly-command-map (kbd "'") 'zap-up-to-char)
+(define-key xah-fly-command-map (kbd "b") 'zap-up-to-char)
 (global-set-key (kbd "<f2>") 'string-rectangle)
 (global-set-key (kbd "<f5>") 'repeat-complex-command)
 ;; (global-set-key (kbd "<f6>") 'xah-open-in-vscode)
