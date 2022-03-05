cb3b44fee8ed06868bb081daf41d5199ce434f70
Andrew Li <andrewli-1920@hotmail.com>
2022-03-05 21:58:56 +0800

    removed all instances of packages for startup performance and debloating.

--- .emacs.d/custom_vars.el
+++ .emacs.d/custom_vars.el
@@ -8,7 +8,7 @@
    '("16ab866312f1bd47d1304b303145f339eac46bbc8d655c9bfa423b957aa23cc9" "5d59bd44c5a875566348fa44ee01c98c1d72369dc531c1c5458b0864841f887c" default))
  '(linum-format " %5i ")
  '(package-selected-packages
-   '(tuareg rg naysayer-theme typescript-mode yaml-mode xah-fly-keys xah-find which-key use-package try toml-mode s rust-mode go-mode git-commit company command-log-mode)))
+   '(tuareg naysayer-theme typescript-mode yaml-mode xah-fly-keys xah-find which-key use-package try toml-mode rust-mode git-commit company command-log-mode)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,8 @@
 ;; -*- lexical-binding: t; -*-
-(setq gc-cons-threshold 100000000)
-(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 800000)))
+(setq gc-cons-threshold 10000000)
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(scroll-bar-mode -1)
 
 (setq ido-case-fold nil)
 (setq case-fold-search nil)
@@ -20,18 +18,15 @@
 (setq scroll-conservatively 1)
 
 (defun efs/display-startup-time ()
-  (message "Emacs loaded in %s with %d garbage collections."
-	   (format "%.2f seconds"
-		   (float-time
-		    (time-subtract after-init-time before-init-time)))
-	   gcs-done))
+    (message "Emacs loaded in %s with %d garbage collections and %d features loaded."
+             (format "%.2f seconds"
+                     (float-time
+                       (time-subtract after-init-time before-init-time)))
+             gcs-done (length features)))
+
 (add-hook 'emacs-startup-hook #'efs/display-startup-time)
 
-;; inhibit startup screen if a file is passed to it.
-(defun my-inhibit-startup-screen-always ()
-  (ignore (setq inhibit-startup-screen t)))
 
-(add-hook 'command-line-functions #'my-inhibit-startup-screen-always)
 
 (defadvice split-window (after move-point-to-new-window activate)
   "Moves the point to the newly created window after splitting."
@@ -110,8 +105,6 @@
         (?\".?\")
         (?\{.?\})))
 
-(rg-use-old-defaults)
-
 ;; i-search with more intuitive controls 
 (progn
   (define-key isearch-mode-map (kbd "<up>")`isearch-ring-retreat)
@@ -126,14 +119,6 @@
 (setq backup-by-copying t)
 (setq auto-save-default nil)
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-
-(add-to-list 'auto-mode-alist '("\\.ts\\'" . javascript-mode))
-(add-to-list 'auto-mode-alist '("\\.cs\\'" . java-mode))
-
-(add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
-
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 (setq xah-fly-use-control-key nil)
@@ -143,38 +128,54 @@
 
 (global-set-key (kbd "`") 'xah-fly-command-mode-activate) ;; if I need to backtick then alt-9-6 I guess
 
+(add-to-list 'default-frame-alist '(foreground-color . "#d1b897"))
+(add-to-list 'default-frame-alist '(background-color . "#062329"))
+(set-face-attribute 'font-lock-builtin-face nil :foreground "#ffffff")
+(set-face-attribute 'font-lock-comment-face nil :foreground "#44b340")
+(set-face-attribute 'font-lock-constant-face nil :foreground "#44b340")
+(set-face-attribute 'font-lock-function-name-face nil :foreground "#ffffff")
+(set-face-attribute 'font-lock-keyword-face nil :foreground "#ffffff")
+(set-face-attribute 'font-lock-type-face nil :foreground "#8cde94")
+(set-face-attribute 'font-lock-string-face nil :foreground "#2ec09c")
+(set-face-attribute 'font-lock-variable-name-face nil :foreground "#c1d1e3")
+
 (set-face-attribute 'fringe nil :background nil)
 (global-font-lock-mode t)
 (setq font-lock-maximum-decoration t)
 
-(require 'font-lock)
-
-(load-theme 'naysayer t)
-
 (setq compile-command "build.bat")
 (define-key xah-fly-command-map (kbd "q") 'goto-line)
 (define-key xah-fly-command-map (kbd "b") 'zap-up-to-char)
+
+(global-set-key (kbd "<f1>") 'isearch-forward-symbol-at-point)
 (global-set-key (kbd "<f2>") 'string-rectangle)
+;; <f3>
+;; <f4>
 (global-set-key (kbd "<f5>") 'repeat-complex-command)
-;; (global-set-key (kbd "<f6>") 'xah-open-in-vscode)
 (global-set-key (kbd "<f6>") 'indent-rigidly)
-(global-set-key (kbd "<f10>") 'align-regexp) 
-(global-set-key (kbd "<f12>") 'isearch-forward-symbol-at-point)
+;; <f7>
+;; <f8>
+;; <f9> 
+;; <f10>
+;; <f11>
+(global-set-key (kbd "<f12>") 'align-regexp) 
+
+(global-set-key (kbd "M-q") 'compile)
+(global-set-key (kbd "M-s") 'move-line-down)
+(global-set-key (kbd "M-w") 'move-line-up)
+(global-set-key (kbd "M-]") 'forward-paragraph)
+(global-set-key (kbd "M-[") 'backward-paragraph)
+
+(global-set-key (kbd "C-b") 'backward-char)
+(global-set-key (kbd "C-f") 'forward-char)
+(global-set-key (kbd "C-n") 'next-line)
+(global-set-key (kbd "C-p") 'previous-line)        
+(global-set-key (kbd "C-s") 'save-buffer)        
 
 (global-set-key (kbd "<kp-8>") 'xah-next-user-buffer)
 (global-set-key (kbd "<kp-2>") 'xah-previous-user-buffer)
-    
-(global-set-key (kbd "M-q") 'compile)
-(global-set-key (kbd "M-]") 'forward-paragraph)
-(global-set-key (kbd "M-[") 'backward-paragraph)
-(global-set-key (kbd "M-w") 'move-line-up)
-(global-set-key (kbd "M-s") 'move-line-down)
-
-(global-set-key (kbd "C-s") 'save-buffer)        
-(global-set-key (kbd "C-p") 'previous-line)        
-(global-set-key (kbd "C-n") 'next-line)
-(global-set-key (kbd "C-f") 'forward-char)
-(global-set-key (kbd "C-b") 'backward-char)
 
 (setq custom-file (locate-user-emacs-file "custom_vars.el"))
 (load custom-file 'noerror 'message) 
+
+(setq gc-cons-threshold (* 2 1000 000))
